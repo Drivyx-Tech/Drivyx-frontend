af3b2d22fdb8640af011b17c6a9f6c57c4a1f98c
YumeLoL <yumeee.lol@gmail.com>
2023-11-04 15:16:14 +1100

    hotfix

--- app/(site)/provider.tsx
+++ app/(site)/provider.tsx
@@ -4,6 +4,8 @@
 import theme from "@/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
