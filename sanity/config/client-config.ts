da59300019393c490e1f52bff10095281991f984
YumeLoL <yumeee.lol@gmail.com>
2023-11-04 16:40:49 +1100

    hotfix: sanity security comment out

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -8,4 +8,4 @@ export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION as string;
 export const useCdn = false;
 // This is the document id used for the preview secret that's stored in your dataset.
 // The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
-export const previewSecretId = process.env.SANITY_REVALIDATE_SECRET as string;
+// export const previewSecretId = process.env.SANITY_REVALIDATE_SECRET as string;
