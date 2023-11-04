67037e91f24db0329047d7543561d5c799350933
YumeLoL <71587684+YumeLoL@users.noreply.github.com>
2023-11-04 15:08:44 +1100

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export async function GET(request: Request) {
   return new Response("Hello, Next.js!");
