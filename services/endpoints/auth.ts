31f65276811828c14bea48923061459fbcd7dab0
YumeLoL <yumeee.lol@gmail.com>
2023-11-19 12:43:00 +1100

    test

--- services/endpoints/auth.ts
+++ services/endpoints/auth.ts
@@ -3,21 +3,30 @@ import { TConfirmSignup, TSignin, TSignup } from "./type";
 
 // signup
 export const signup = (data: TSignup["requestType"]) => {
-  return Fetcher.init<TSignup>("POST", "/signup")
+  return Fetcher.init<TSignup>(
+    "POST",
+    "https://8b9990jfmk.execute-api.ap-southeast-2.amazonaws.com/dev/signup"
+  )
     .withJsonPaylad(data)
     .fetchData();
 };
 
 // confirm-signup
 export const confirmSignup = (data: TConfirmSignup["requestType"]) => {
-  return Fetcher.init<TConfirmSignup>("POST", "/confirm-signup")
+  return Fetcher.init<TConfirmSignup>(
+    "POST",
+    "https://8b9990jfmk.execute-api.ap-southeast-2.amazonaws.com/dev/confirm-signup"
+  )
     .withJsonPaylad(data)
     .fetchData();
 };
 
 // signin
 export const signin = (data: TSignin["requestType"]) => {
-  return Fetcher.init<TSignin>("POST", "/signin")
+  return Fetcher.init<TSignin>(
+    "POST",
+    "https://8b9990jfmk.execute-api.ap-southeast-2.amazonaws.com/dev/signin"
+  )
     .withJsonPaylad(data)
     .fetchData();
 };
