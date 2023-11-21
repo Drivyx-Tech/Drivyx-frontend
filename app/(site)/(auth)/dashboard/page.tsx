0a3b883b95e91356210f6b67b586a4de8900f2ed
YumeLoL <yumeee.lol@gmail.com>
2023-11-21 11:03:20 +1100

    chore: move profile page to /dashboard/profile

--- app/(site)/(auth)/dashboard/page.tsx
+++ app/(site)/(auth)/dashboard/page.tsx
@@ -1,36 +1,11 @@
-// "use client";
+import { Flex, Text } from "@chakra-ui/react";
 
-import { Flex, Grid } from "@chakra-ui/react";
-import { User } from "@/services/endpoints/type";
-import { getUser } from "@/services/endpoints/user";
 import React from "react";
-import ProfileHeader from "@/components/Profile/ProfileHeader";
-import avatar4 from "@/public/img/avatars/avatar4.png";
-import ProfileInformation from "@/components/Profile/ProfileInformation";
 
 async function Dashboard() {
-  // const [user, setUser] = useState<User>();
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const user = await getUser();
-  //     // Check if window is defined before accessing localStorage
-  //     if (typeof window !== "undefined") {
-  //       console.log("user info", user.result);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
   return (
     <Flex direction="column" h={"100vh"}>
-      <ProfileHeader
-        // avatarImage={avatar4.src}
-        name={"Esthera Jackson"}
-        email={"esthera@simmmple.com"}
-      />
-      <ProfileInformation />
+      <Text>Dashboard</Text>
     </Flex>
   );
 }
--- app/(site)/(auth)/dashboard/profile/page.tsx
+++ app/(site)/(auth)/dashboard/profile/page.tsx
@@ -1,7 +1,32 @@
+import ProfileHeader from "@/components/Profile/ProfileHeader";
+import ProfileInformation from "@/components/Profile/ProfileInformation";
+import { Flex } from "@chakra-ui/react";
 import React from "react";
 
 function Profile() {
-  return <div>Profile</div>;
+  // const [user, setUser] = useState<User>();
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const user = await getUser();
+  //     // Check if window is defined before accessing localStorage
+  //     if (typeof window !== "undefined") {
+  //       console.log("user info", user.result);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+  return (
+    <Flex direction="column" h={"100vh"}>
+      <ProfileHeader
+        // avatarImage={avatar4.src}
+        name={"Esthera Jackson"}
+        email={"esthera@simmmple.com"}
+      />
+      <ProfileInformation />
+    </Flex>
+  );
 }
 
 export default Profile;
