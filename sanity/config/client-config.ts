9a6b5a436b17bfc206b3c91052f544ae40225d0f
YumeLoL <yumeee.lol@gmail.com>
2024-01-09 20:14:54 +1100

    change useCdn

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,4 +1,4 @@
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION as string;
-export const useCdn = false as boolean;
+export const useCdn = true as boolean;
