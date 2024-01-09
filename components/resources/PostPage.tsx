f8a60005ef0fa9fc9c3ad7ae10a0fab3e4347759
YumeLoL <yumeee.lol@gmail.com>
2024-01-09 21:07:37 +1100

    false useCdn

--- components/resources/PostPage.tsx
+++ components/resources/PostPage.tsx
@@ -7,10 +7,10 @@ import {
   HStack,
   OrderedList,
   ListItem,
-  Divider,
   Image,
   UnorderedList,
   Link,
+  Divider,
   Stack,
   Flex,
 } from "@chakra-ui/react";
@@ -91,126 +91,22 @@ function PostPage(props: any) {
               boxSize="40px"
               alt={`Avatar of ${props.name}`}
             />
-            <Text>Author: {post.author?.name}</Text>
-            <Text>—</Text>
-            <Text> {format(parseISO(post?.publishedAt), "MMMM dd, yyyy")}</Text>
+            <VStack gap={0} align={"left"}>
+              <Text fontWeight={"bold"} fontSize="sm">
+                {post.author?.name}
+              </Text>
+              <Text fontSize={"sm"}>
+                {" "}
+                {format(parseISO(post?.publishedAt), "MMMM dd, yyyy")}
+              </Text>
+            </VStack>
           </HStack>
 
-          {/* <Text as="p" fontSize="xl" mt={20} mb={6}> */}
+          <Divider />
+
           {post.body && (
             <PortableText value={post.body} components={CustomPortableText} />
           )}
-          {/* </Text> */}
-
-          <Heading as="h2" size="md" mb={2}>
-            The Present: Drivyx Beta Phase
-          </Heading>
-          <Text as="p" fontSize="xl" mb={6}>
-            In this beta phase, our primary goal is to create a robust and
-            dynamic platform where projects dedicated to environmental, social,
-            and governance (ESG) goals can present their work to a global
-            audience. This phase is crucial for us to gather feedback, fine-tune
-            functionalities, and ensure that Drivyx not only meets but exceeds
-            the expectations of our users. We invite project owners to join us
-            in this foundational stage, where every feedback and interaction
-            shapes the future of Drivyx.
-          </Text>
-
-          <Heading as="h2" size="md" mb={2}>
-            The Future: Blockchain and AI in 2024
-          </Heading>
-          <Heading as="h2" size="md" mb={2} fontWeight={"500"}>
-            Looking ahead to 2024, Drivyx is set to embark on an exciting phase
-            of technological integration:
-          </Heading>
-
-          <OrderedList as="p" fontSize="xl" mb={6}>
-            <ListItem>
-              Blockchain Technology:
-              <UnorderedList>
-                <ListItem>
-                  Transparency and Trust: With blockchain, every transaction and
-                  investment on Drivyx will be transparent, secure, and
-                  tamper-proof. This technology fosters trust among users and
-                  ensures accountability for all parties involved.
-                </ListItem>
-                <ListItem>
-                  Smart Contracts: The use of smart contracts will streamline
-                  processes, making investment transactions seamless, efficient,
-                  and automated.
-                </ListItem>
-              </UnorderedList>
-            </ListItem>
-            <ListItem>
-              AI Integration:
-              <UnorderedList>
-                <ListItem>
-                  Enhanced Project Matching: AI algorithms will analyze investor
-                  preferences and project requirements, facilitating perfect
-                  matches between ESG projects and potential investors.
-                </ListItem>
-                <ListItem>
-                  Data-Driven Insights: Leveraging AI, Drivyx will provide
-                  valuable insights into market trends, project impacts, and
-                  investment opportunities, empowering users to make informed
-                  decisions.
-                </ListItem>
-              </UnorderedList>
-            </ListItem>
-          </OrderedList>
-
-          <Heading as="h2" size="md" mb={2}>
-            Your Role in This Community:
-          </Heading>
-          <Text as="p" fontSize="xl" mb={6}>
-            By joining Drivyx, you're not just participating in a platform;
-            you're becoming part of a movement. Whether you're a project leader
-            seeking funding or an investor looking for sustainable ventures,
-            Drivyx is where your journey begins.
-          </Text>
-
-          <Heading as="h2" size="md" mb={2}>
-            How to Get Involved:
-          </Heading>
-          <OrderedList as="p" fontSize="xl" mb={6}>
-            <ListItem>xx</ListItem>;
-          </OrderedList>
-
-          <Heading as="h2" size="md" mb={2}>
-            Join Us on This Exciting Journey
-          </Heading>
-          <Text as="p" fontSize="xl" mb={6}>
-            As we progress through our beta phase and look forward to the
-            integration of blockchain and AI, we invite you to be a part of this
-            revolutionary journey. Your participation and feedback are vital to
-            us. Whether you're a project owner, an investor, or an enthusiast in
-            the ESG space, your engagement with Drivyx today will contribute
-            significantly to the shape of our platform tomorrow.
-          </Text>
-
-          <Heading as="h2" size="md" mb={2}>
-            Start Now:
-          </Heading>
-          <Text as="p" fontSize="xl" mb={6}>
-            We encourage you to visit Drivyx, explore the projects that are
-            already making a difference, and share your valuable feedback. Your
-            insights will be instrumental in enhancing the platform's features
-            and usability. Together, let’s build a future where technology and
-            sustainability go hand in hand for a better world.
-          </Text>
-
-          <Text as={"i"}>
-            Discover the future of ESG investing. Visit
-            <Link
-              href={"https://www.drivyx.com/"}
-              fontWeight={"bold"}
-              color={"primary.500"}
-            >
-              {" "}
-              Drivyx
-            </Link>{" "}
-            now and be a part of our growth story.
-          </Text>
         </VStack>
       </Container>
     </Stack>
--- ui/PortableTextComponents/CustomPortableText.tsx
+++ ui/PortableTextComponents/CustomPortableText.tsx
@@ -1,17 +1,17 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Link, ListItem } from "@chakra-ui/react";
 
 export const CustomPortableText = {
   block: {
     h1: ({ children }: any) => {
       return (
-        <Text fontSize={"28px"} color={"secondary.800"} fontWeight={"bold"}>
+        <Text fontSize={"36px"} color={"secondary.800"} fontWeight={"bold"}>
           {children}
         </Text>
       );
     },
     h2: ({ children }: any) => {
       return (
-        <Text fontSize={"26px"} color={"secondary.800"} fontWeight={"bold"}>
+        <Text fontSize={"28px"} color={"secondary.800"} fontWeight={"bold"}>
           {children}
         </Text>
       );
@@ -25,10 +25,67 @@ export const CustomPortableText = {
     },
     h4: ({ children }: any) => {
       return (
-        <Text fontSize={"22px"} color={"secondary.800"} fontWeight={"bold"}>
+        <Text fontSize={"20px"} color={"secondary.800"} fontWeight={"bold"}>
           {children}
         </Text>
       );
     },
+    normal: ({ children }: any) => {
+      return (
+        <Text fontSize={"18px"} color={"secondary.800"}>
+          {children}
+        </Text>
+      );
+    },
+  },
+  marks: {
+    emphasis: ({ children }: any) => (
+      <Text as="span" fontStyle="italic">
+        {children}
+      </Text>
+    ),
+    underline: ({ children }: any) => (
+      <Text as="span" textDecoration="underline">
+        {children}
+      </Text>
+    ),
+    "strike-through": ({ children }: any) => (
+      <Text as="span" textDecoration="line-through">
+        {children}
+      </Text>
+    ),
+    strong: ({ children }: any) => (
+      <Text as="span" fontWeight="bold">
+        {children}
+      </Text>
+    ),
+    url: ({ mark, children }: any) => (
+      <Link
+        href={mark.href}
+        fontWeight={"bold"}
+        color={"primary.500"}
+        textDecoration="underline"
+      >
+        {children}
+      </Link>
+    ),
+  },
+  lists: {
+    bullet: ({ children }: any) => (
+      <ListItem color={"secondary.800"}>{children}</ListItem>
+    ),
+    numbered: ({ children }: any) => (
+      <ListItem color={"secondary.800"}>{children}</ListItem>
+    ),
+    url: ({ mark, children }: any) => (
+      <Link
+        href={mark.href}
+        fontWeight={"bold"}
+        color={"primary.500"}
+        textDecoration="underline"
+      >
+        {children}
+      </Link>
+    ),
   },
 };
