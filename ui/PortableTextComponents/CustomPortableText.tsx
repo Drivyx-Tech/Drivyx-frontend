26e7d4fc9db25c772aa9afbfe74cb019183c30b2
YumeLoL <yumeee.lol@gmail.com>
2024-01-10 00:14:54 +1100

    sanity portable text

--- ui/PortableTextComponents/CustomPortableText.tsx
+++ ui/PortableTextComponents/CustomPortableText.tsx
@@ -43,28 +43,6 @@ export const CustomPortableText = {
         </Text>
       );
     },
-    lists: {
-      bullet: ({ children }: any) => (
-        <UnorderedList>
-          <ListItem color={"secondary.800"}>{children}</ListItem>
-        </UnorderedList>
-      ),
-      numbered: ({ children }: any) => (
-        <OrderedList>
-          <ListItem color={"secondary.800"}>{children}</ListItem>
-        </OrderedList>
-      ),
-      url: ({ mark, children }: any) => (
-        <Link
-          href={mark.href}
-          fontWeight={"bold"}
-          color={"primary.500"}
-          textDecoration="underline"
-        >
-          {children}
-        </Link>
-      ),
-    },
   },
   marks: {
     emphasis: ({ children }: any) => (
@@ -87,15 +65,27 @@ export const CustomPortableText = {
         {children}
       </Text>
     ),
-    url: ({ mark, children }: any) => (
+    link: ({ value, children }: any) => (
       <Link
-        href={mark.href}
+        href={value.href}
         fontWeight={"bold"}
         color={"primary.500"}
         textDecoration="underline"
       >
         {children}
       </Link>
+    ),
+  },
+  lists: {
+    bullet: ({ children }: any) => (
+      <UnorderedList>
+        <ListItem color={"secondary.800"}>{children}</ListItem>
+      </UnorderedList>
+    ),
+    numbered: ({ children }: any) => (
+      <OrderedList>
+        <ListItem color={"secondary.800"}>{children}</ListItem>
+      </OrderedList>
     ),
   },
 };
